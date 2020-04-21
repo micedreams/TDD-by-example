3a734c03645a4846243f4b1d4a1eaff16e472fb6
micedreams <micedreams@gmail.com>
2020-04-21 21:19:39 +0530

    degenerate objects

--- spec/unit/application/dollar_spec.rb
+++ spec/unit/application/dollar_spec.rb
@@ -3,7 +3,15 @@ require "spec_helper"
 describe "Unit Test of Application" do
   it "multiplication" do
     five = Dollar.new(5)
-    five.times(2)
-    expect(five.amount).to eq(10)
+    product = five.times(2)
+    expect(product.amount).to eq(10)
+  end
+
+  it "test another multiplication" do
+    five = Dollar.new(5)
+    product = five.times(2)
+    expect(product.amount).to eq(10)
+    product = five.times(3)
+    expect(product.amount).to eq(15)
   end
 end
